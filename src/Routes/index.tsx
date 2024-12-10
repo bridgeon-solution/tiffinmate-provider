b7d6775241fd121c0b69bfa64e8ee9bf4f7a8b11
Mohammed shuhaib <shuhaib@MacBookAir.mshome.net>
2024-12-10 21:54:34 +0530

    fixes

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from '../Pages/Signup'
 import Login from '../Pages/Login'
+import Dashboards from '../Pages/Dashboards'
+import Orderlist from '../Pages/Orderlist'
 
 function AppRouter() {
   return (
@@ -8,7 +10,8 @@ function AppRouter() {
     <Routes>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
-        
+        <Route path='/Dashboard' element={<Dashboards/>} />
+        <Route path='/Orderlist' element={<Orderlist/>} />
     </Routes>
     </BrowserRouter>
   )
